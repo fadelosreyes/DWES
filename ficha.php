fdf249fb2a6346b0383a5ae584431714ec1c71d0
Francisco de asis <franciscodeasis.delosreyes@iesdonana.org>
2024-09-26 21:12:51 +0200

    .

--- ficha.php
+++ ficha.php
@@ -6,15 +6,28 @@
     <title>Document</title>
 </head>
 <body>
+    <?= $_POST['nombre'] ?>
+    <p>
     <?php
-    $x = 25;
-    $y = 30;
-    $z = $y + $x;
-    $n = true;
-    $m = false;
+    //$x = 25;
+    //$y = 30;
+    //$z = $y + $x;
+    //$n = true;
+    //$m = false;
+    //echo $y;
+    //$a = array('a', 'b', 'c', 'd', 'e', 'f');
+    //print_r($a);
+    //echo "<br>";
+    //$b = ["gato" => "cat", "perro" => "dog"];
+    //echo $b['gato'];
+    //echo "<br>";
+    //$a[] = 'hola';
+    //print_r($a);
+    //echo "<br>";
+    //$a[7] = 'hHola';
+    //print_r($a);
+    //echo "<br>";
     ?>
-    <p>Esto es el resultado del formulario</p>
-    ayukcchba bcaga
-    <?= $z ?>
+    </p>
 </body>
 </html>
